31b2211f1a67aa8f801498fd4f6b6e69e36d871b
ramawahyu112 <ramawahyudana112@gmail.com>
2022-05-29 21:25:34 +0800

    js logic flow challenge : data-customer

--- index.js
+++ index.js
@@ -23,19 +23,33 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  let i = 0;
+  while(i < dataYangAkanDilooping.length){
+    hasilLooping.push(dataYangAkanDilooping[i].namaDepan +' ' + dataYangAkanDilooping[i].namaBelakang)
+    i++;
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
-
+  for(let i = 0;i<dataYangAkanDilooping.length;i++){
+    if(dataYangAkanDilooping[i].jenisKelamin == "M"){
+      jumlahPria ++;
+    }
+  }
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
+  for(let i = 0;i<dataYangAkanDilooping.length;i++){
+    if(dataYangAkanDilooping[i].jenisKelamin == "F"){
+      jumlahWanita ++;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +63,13 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if(jumlahPria>jumlahWanita){
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahPria<jumlahWanita){
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else if (jumlahPria==jumlahWanita) {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +86,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
